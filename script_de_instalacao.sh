864a2894bad864e7bd44756db58df5772cfbb9a3
cmgsi <cmgsi@hotmail.com>
2019-09-29 20:59:13 -0300

    script de instalação

---
+++ script_de_instalacao.sh
@@ -0,0 +1,11 @@
+# 1 - Instalar Node js
+	sudo apt update
+	sudo apt install nodejs
+	sudo apt install npm
+	nodejs -v
+	npm -v
+
+# 2 - Instalar o VSCode
+	https://tecadmin.net/install-visual-studio-code-editor-ubuntu/
+
+	
